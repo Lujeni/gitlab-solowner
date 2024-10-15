7508c63e4e08e040d5790512c34330fcd5dfa985
Lujeni <julien@thebault.co>
2024-10-15 16:04:28 +0200

    feat(inactivity): support pagination

--- inactivity.exs
+++ inactivity.exs
@@ -4,24 +4,39 @@ Mix.install([
   {:jason, "~> 1.4"}
 ])
 
-one_year_ago = DateTime.utc_now() |> DateTime.add(-365 * 24 * 60 * 60, :second)
+defmodule GitlabSolowners do
+  def paginate_projects(page \\ 1) do
+    one_year_ago = DateTime.utc_now() |> DateTime.add(-365 * 24 * 60 * 60, :second)
 
-"https://gitlab.com/api/v4/projects"
-|> Req.get!()
-|> Map.get(:body)
-|> Enum.each(fn project -> 
-  "https://gitlab.com/api/v4/projects/#{project["id"]}/repository/commits?per_page=1"
-  |> Req.get!()
-  |> Map.get(:body)
-  |> Enum.each(fn commit -> 
-    commit_datetime = commit["created_at"] |> DateTime.from_iso8601()
-    case commit_datetime do
-      {:ok, datetime, _offset} -> 
-        if DateTime.compare(datetime, one_year_ago) == :lt do
-          IO.puts("Commit from #{commit["created_at"]} is older than one year.")
+    response = Req.get!("https://gitlab.com/api/v4/projects?page=#{page}")
+    response
+    |> Map.get(:body)
+    |> Enum.each(fn project ->
+      IO.puts("fetch #{project["name"]}")
+
+      "https://gitlab.com/api/v4/projects/#{project["id"]}/repository/commits?per_page=1"
+      |> Req.get!()
+      |> Map.get(:body)
+      |> Enum.each(fn commit ->
+        commit_datetime = commit["created_at"] |> DateTime.from_iso8601()
+        case commit_datetime do
+          {:ok, datetime, _offset} ->
+            if DateTime.compare(datetime, one_year_ago) == :lt do
+              IO.puts("Commit from #{commit["created_at"]} is older than one year.")
+            end
+          _error ->
+            IO.puts("Error parsing date: #{commit["created_at"]}")
         end
-      _error -> 
-        IO.puts("Error parsing date: #{commit["created_at"]}")
+      end)
+    end)
+
+    headers = response.headers
+    next_page = hd(headers["x-next-page"])
+    if next_page  do
+      paginate_projects(next_page)
     end
-  end)
-end)
+  end
+end
+
+# Start pagination from the first page
+GitlabSolowners.paginate_projects()
