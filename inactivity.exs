502c6c7eeb741453dea4f58f7d0bc2551375cf99
Lujeni <julien@thebault.co>
2024-10-16 13:41:01 +0200

    feat(inactivity): silly errors handling

--- inactivity.exs
+++ inactivity.exs
@@ -9,14 +9,9 @@ defmodule GitlabSolowners do
   @gitlab_api_token System.get_env("GITLAB_API_TOKEN")
   @one_year_ago DateTime.utc_now() |> DateTime.add(-365 * 24 * 60 * 60, :second)
 
-  def paginate_projects(page \\ 1) do
-
-    response = Req.get!("#{@gitlab_api_url}/projects?page=#{page}&per_page=1000", auth: {:bearer, @gitlab_api_token})
-    response
-    |> Map.get(:body)
-    |> Enum.each(fn project ->
-      "#{@gitlab_api_url}/projects/#{project["id"]}/repository/commits?per_page=1"
-      |> Req.get!(auth: {:bearer, @gitlab_api_token})
+  def check_commit(req, project) do
+    try do
+      Req.get!(req, url: "/projects/#{project["id"]}/repository/commits?per_page=1")
       |> Map.get(:body)
       |> Enum.each(fn commit ->
         commit_datetime = commit["created_at"] |> DateTime.from_iso8601()
@@ -29,15 +24,30 @@ defmodule GitlabSolowners do
             IO.puts("Error parsing date: #{commit["created_at"]}")
         end
       end)
+    rescue
+      e -> IO.puts("Error retrieve commits from #{project["path"]} #{inspect(e)}")
+    end
+  end
+
+  def get_projects(req, page \\ 1) do
+    response = Req.get!(req, url: "/projects?page=#{page}&per_page=1000")
+    response
+    |> Map.get(:body)
+    |> Enum.each(fn project ->
+      check_commit(req, project)
     end)
 
     headers = response.headers
     next_page = hd(headers["x-next-page"])
     if next_page  do
-      paginate_projects(next_page)
+      get_projects(req, next_page)
     end
+  end
+
+  def run() do
+    req = Req.new(base_url: @gitlab_api_url, auth: {:bearer, @gitlab_api_token}, http_errors: :raise)
+    get_projects(req)
   end
 end
 
-# Start pagination from the first page
-GitlabSolowners.paginate_projects()
+GitlabSolowners.run()
