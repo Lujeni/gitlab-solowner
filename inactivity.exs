071f9e2761bd244a1f959ee74874c596b622ba5a
Lujeni <julien@thebault.co>
2024-10-16 11:34:16 +0200

    feat(inactivity): support bearer

--- inactivity.exs
+++ inactivity.exs
@@ -5,26 +5,25 @@ Mix.install([
 ])
 
 defmodule GitlabSolowners do
-  @gitlab_api_url System.get_env("GITLAB_API_URL") || "https://gitlab.com/api/v4/"
+  @gitlab_api_url System.get_env("GITLAB_API_URL") || "https://gitlab.com/api/v4"
+  @gitlab_api_token System.get_env("GITLAB_API_TOKEN")
+  @one_year_ago DateTime.utc_now() |> DateTime.add(-365 * 24 * 60 * 60, :second)
 
   def paginate_projects(page \\ 1) do
-    one_year_ago = DateTime.utc_now() |> DateTime.add(-365 * 24 * 60 * 60, :second)
 
-    response = Req.get!("#{@gitlab_api_url}/projects?page=#{page}")
+    response = Req.get!("#{@gitlab_api_url}/projects?page=#{page}&per_page=1000", auth: {:bearer, @gitlab_api_token})
     response
     |> Map.get(:body)
     |> Enum.each(fn project ->
-      IO.puts("fetch #{project["name"]}")
-
       "#{@gitlab_api_url}/projects/#{project["id"]}/repository/commits?per_page=1"
-      |> Req.get!()
+      |> Req.get!(auth: {:bearer, @gitlab_api_token})
       |> Map.get(:body)
       |> Enum.each(fn commit ->
         commit_datetime = commit["created_at"] |> DateTime.from_iso8601()
         case commit_datetime do
           {:ok, datetime, _offset} ->
-            if DateTime.compare(datetime, one_year_ago) == :lt do
-              IO.puts("Commit from #{commit["created_at"]} is older than one year.")
+            if DateTime.compare(datetime, @one_year_ago) == :lt do
+              IO.puts("#{project["namespace"]["path"]},#{project["path"]},#{commit["created_at"]}")
             end
           _error ->
             IO.puts("Error parsing date: #{commit["created_at"]}")
